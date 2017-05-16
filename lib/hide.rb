d37cb8feffe1e7b6fa0fdf1b98fc64a4d161d7e9
Tahmid Shakil <at.shakil.92@gmail.com>
2017-05-17 04:09:03 +0600

    API update

--- lib/hide.rb
+++ lib/hide.rb
@@ -1,29 +1,28 @@
 require "openssl"
 require "securerandom"
+require "hide/ae"
 
-class Hide
-  class << self
-    def encrypt data, key, salt, iter, iv=SecureRandom.random_bytes(16),
-      key_length = 32
-      cipher = OpenSSL::Cipher.new "AES-256-CBC"
-      cipher.encrypt
-      cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, iter, key_length)
-      cipher.iv = iv
-      {
-        data: cipher.update(data) + cipher.final,
-        iv: iv
-      }
-    end
+module Hide
+  module_function
 
-    def decrypt data, key, salt, iter, iv, key_length
-      decipher = OpenSSL::Cipher.new "AES-256-CBC"
-      decipher.decrypt
-      decipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, iter,
-        key_length)
-      decipher.iv = iv
-      decipher.update(data) + decipher.final
-    end
+  def encrypt data, key, salt, iter, iv=SecureRandom.random_bytes(16),
+    key_length = 32
+    cipher = OpenSSL::Cipher.new "AES-256-CBC"
+    cipher.encrypt
+    cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, iter, key_length)
+    cipher.iv = iv
+    {
+      data: cipher.update(data) + cipher.final,
+      iv: iv
+    }
+  end
+
+  def decrypt data, key, salt, iter, iv, key_length
+    decipher = OpenSSL::Cipher.new "AES-256-CBC"
+    decipher.decrypt
+    decipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, iter,
+      key_length)
+    decipher.iv = iv
+    decipher.update(data) + decipher.final
   end
 end
-
-require "hide/ae"
